bbb60aef00fcc18f6792057bb64e6dbeff492ce2
jbest2021 <43757518+jbest2021@users.noreply.github.com>
2018-12-17 19:45:43 -0500

    done

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -238,7 +238,34 @@ function drawSmileyFace() {
 
 function drawStar() {
   let star = document.getElementById('canvas7').getContext('2d');
-
+let outerRadius = Number(prompt("Outer Radius:"));
+  let innerRadius = Number(prompt("Inner Radius:"));
+  let canvas = document.getElementById('canvas6');
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  if (isNaN(outerRadius) == true || isNaN(innerRadius) == true) {
+    alert("One of your inputs is not a number.");
+  } else if (outerRadius < 2) {
+    alert("Your outer radius is too small.");
+  } else if (innerRadius < 1) {
+    alert("Your inner radius is too small.");
+  } else if (outerRadius <= innerRadius) {
+    alert("Your outer radius must be larger than your inner radius.")
+  } else {
+    context.beginPath();
+    context.moveTo(125, 125 - outerRadius);
+   let x = 0;
+    let angle = 0 * Math.PI;
+    while (x < 5) {
+      context.lineTo(Math.cos(1.3 * Math.PI - angle) * innerRadius + 125, Math.sin(1.3 * Math.PI - angle) * innerRadius + 125);
+      context.lineTo(Math.cos(1.1 * Math.PI - angle) * outerRadius + 125, Math.sin(1.1 * Math.PI - angle) * outerRadius + 125);
+      angle = angle + 0.4 * Math.PI;
+      x = x + 1;
+  }
+  context.closePath();
+  context.stroke();
+  lineWidth = 1;
+}
 }
 
 /*
@@ -257,8 +284,37 @@ function drawStar() {
  */
 
 function drawStopSign() {
-  let stop = document.getElementById('canvas7').getContext('2d');
-// 65 pixel san serif
+  let canvas = document.getElementById('canvas7');
+  let ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  let sideLength=80;
+  let center=[10+(sideLength)/2+sideLength/Math.sqrt(2), 10+(sideLength/2)+(sideLength/Math.sqrt(2))]
+  console.log(center)
+  let points=8;
+  let pointx=[];
+  let pointy=[];
+
+  for(let a=0;a<points;a++){
+    pointx.push(Math.cos(((Math.PI*2*a)/points)-Math.PI/8)*100+center[0]);
+    pointy.push(Math.sin(((Math.PI*2*a)/points)-Math.PI/8)*100+center[1]);
+  }
+  ctx.beginPath();
+  ctx.moveTo([pointx][0], pointy[0]);
+  for(let y=0;y<pointx.length;y++){
+    ctx.lineTo(pointx[y], pointy[y]);
+  }
+  ctx.lineTo(pointx[0], pointy[0]);
+  ctx.stroke();
+
+  ctx.fillStyle="red";
+  ctx.fill();
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.textAlign="center";
+  ctx.font="65px sans-serif";
+  ctx.fillStyle="white";
+  ctx.fillText("STOP", center[0], center[1]+15);
+  ctx.closePath()
 }
 
 /*
@@ -280,7 +336,32 @@ function drawStopSign() {
  */
 
 function drawPyramid() {
-
+const canvas = document.getElementById('canvas8');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  let length=Number(prompt("Length:"));
+  if(isNaN(length)==true){
+    alert("Your input is not a number.");
+  }else if(length>100){
+    alert("The pyramid will not fit on the canvas.")
+  }else{
+    ctx.beginPath();
+    let x=10;
+    let prevx=10;
+    let y=canvas.height-10-length;
+    for(let b = 0; b<5; b++){
+    for(let a = 0; a<5-b; a++){
+      ctx.moveTo(x,y);
+      ctx.rect(x,y,length,length);
+      x+=length;
+    }
+    y-=length
+    prevx+=length/2;
+    x=prevx;
+    }
+    ctx.lineWidth=1;
+    ctx.stroke();
+  }
 }
 
 /*
@@ -313,6 +394,53 @@ function drawPyramid() {
  */
 
 function drawHouse() {
-  let house = document.getElementById('canvas7').getContext('2d');
-
+  const canvas = document.getElementById("canvas9");
+  const context = canvas.getContext("2d");
+  let houseColor = prompt("House Color:");
+  let doorColor = prompt("Front Door Color:");
+  if(houseColor!="blue" && houseColor!="brown" && houseColor!="green" && houseColor!="orange" && houseColor!="purple" && houseColor!="red" && houseColor!="yellow" || doorColor!="blue" && doorColor!="brown" && doorColor!="green" && doorColor!="orange" && doorColor!="purple" && doorColor!="red" && doorColor!="yellow"){
+    alert("One of your colors is not supported.")
+  }
+  else{
+    let houseHeight = (canvas.height / 5) * 3;
+    let houseWidth = canvas.width - 300;
+    context.rect(150, canvas.height-10-houseHeight, houseWidth, houseHeight);
+    
+    context.fillStyle = houseColor;
+    context.fill();
+    context.strokeStyle = "black";
+    context.stroke();
+    context.beginPath();
+    context.moveTo(512, 10);
+    context.lineTo(151,294);
+    context.lineTo(873,294);
+    context.closePath();
+    context.lineWidth = 2;
+    context.stroke();
+    
+    context.fillStyle = "gray";
+    context.fill();
+    
+    context.fillStyle = doorColor;
+    context.fillRect(450, 540, 124, 210);
+    context.strokeRect(450,540,124,210);
+    context.lineWidth = 2;
+    context.beginPath();
+    context.moveTo(568, 645);
+    context.arc(560, 645, 8, 0, Math.PI*2);
+    context.stroke();
+    
+    context.fillStyle = "gold";
+    context.fill();
+    
+    context.fillStyle = "lightBlue";
+    context.fillRect(250, 367, 100, 100);
+    context.strokeRect(250, 367, 100, 100);
+    context.fillRect(250, canvas.height-185, 100, 100);
+    context.strokeRect(250, canvas.height-185, 100, 100);
+    context.fillRect(674, 367, 100, 100);
+    context.strokeRect(674, 367, 100, 100);
+    context.fillRect(674, canvas.height-185, 100, 100);
+    context.strokeRect(674, canvas.height-185, 100, 100);
+  }
 }
